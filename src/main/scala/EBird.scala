9e6ce6f7c7605a28ef8595b50f470642186392c9
vikas <vikas@github.ccs.neu.edu>
2017-04-20 18:51:20 -0400

    end to end run completed

--- src/main/scala/EBird.scala
+++ src/main/scala/EBird.scala
@@ -3,6 +3,8 @@
   */
 
 import org.apache.spark.{SparkConf,SparkContext}
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.types.DoubleType;
 import org.apache.spark.mllib.tree.RandomForest
 import org.apache.spark.mllib.tree.model.RandomForestModel
 import org.apache.spark.mllib.util.MLUtils
@@ -11,27 +13,41 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType}
+import org.apache.spark.sql.functions._
+import com.google.common.collect.ImmutableMap;
+import org.apache.spark.sql.functions.countDistinct;
+
 object EBird {
 
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("PageRank").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
     // Load and parse the data file.
-    //val data = sc.textFile("/Users/vikasjanardhanan/courses/mreduce/project/core-covariate_data.csv")
+    val data = sc.textFile("/Users/vikasjanardhanan/courses/mreduce/project/test1.csv")
 
     //val parsedData = data.map { line =>
     //  val parts = line.split(',')
-    //  LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+    //  parts.map(content =>
+    //    if (content == "?")
+    //      0.0
+    //    else
+    //      content)
     //}
+
     //print(parsedData);
 
     val df = sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "true") // Use first line of all files as header
+       // .option("nullValue","?")
       .option("inferSchema", "true") // Automatically infer data types
-      .load("/Users/vikasjanardhanan/courses/mreduce/project/core-covariate_data.csv")
+      .load("/Users/vikasjanardhanan/courses/mreduce/project/test.csv")
 
+    df.show
+    //val df1 = df.na.fill(0.0,df.columns)
+    //df1.show
     val samplingIndexer = new StringIndexer()
       .setInputCol("SAMPLING_EVENT_ID")
       .setOutputCol("SAMPLING_EVENT_ID_INDEX")
@@ -53,37 +69,65 @@ object EBird {
 
     val dfWithBailey = BaileyIndexed.transform(dfLOCID).toDF()
     val finalDF = dfWithBailey.drop("SAMPLING_EVENT_ID").drop("LOC_ID").drop("BAILEY_ECOREGION")
-    finalDF.show
-    //print(df)
-    // Split the data into training and test sets (30% held out for testing)
-    //val splits = df.randomSplit(Array(0.7, 0.3))
-     //val (trainingData, testData) = (splits(0), splits(1))
 
-    // Train a RandomForest model.
-    // Empty categoricalFeaturesInfo indicates all features are continuous.
-     //val numClasses = 2
-     //val categoricalFeaturesInfo = Map[Int, Int]()
-     //val numTrees = 3 // Use more in practice.
-     //val featureSubsetStrategy = "auto" // Let the algorithm choose.
-     //val impurity = "variance"
-     //val maxDepth = 4
-     //val maxBins = 32
+    val finalDFFill = finalDF.na.replace("*",ImmutableMap.of("?", "0.0"))
+      .na.replace("Agelaius_phoeniceus",ImmutableMap.of("X", "1"))
+    //val finalDFFill = finalDF.na.replace("Agelaius_phoeniceus",ImmutableMap.of("X", "1"))
+    //val targedReplaced = finalDFFill.withColumn("Agelaius_phoeniceus",
+    //  when(("bar" == "noValidFormat"), "noValue"))
+    //val updatedDf = finalDF.withColumn("name", regexp_replace(col("name"), ",", ""))
 
-    //val dfRDD = trainingData.rdd
-     //val model = RandomForest.trainRegressor(dfRDD, categoricalFeaturesInfo,
-     //  numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+    //val testDF = finalDFFill.select(finalDFFill("*").cast(DoubleType))
+    val testDF = finalDFFill.columns.foldLeft(finalDFFill)((current, c) => current.withColumn(c, col(c).cast(DoubleType)))
 
-    // // Evaluate model on test instances and compute test error
-    // val labelsAndPredictions = testData.map { point =>
-    //   val prediction = model.predict(point.features)
-    //   (point.label, prediction)
-    // }
-    // val testMSE = labelsAndPredictions.map{ case(v, p) => math.pow((v - p), 2)}.mean()
-    // println("Test Mean Squared Error = " + testMSE)
-    // println("Learned regression forest model:\n" + model.toDebugString)
+    testDF.show
+    print(testDF.schema)
+    // create new dataframe with added column named "notempty"
+    val finalDf = testDF.withColumn("Agelaius_phoeniceus",
+      when($"Agelaius_phoeniceus" > 1.0, 1.0).otherwise(0.0))
+    finalDf.show(100)
+    finalDf.printSchema
+    //print(finalDf.select(finalDf("Agelaius_phoeniceus")).distinct)
+    finalDf.agg(countDistinct("Agelaius_phoeniceus")).toDF().show
+
+
+    val ignored = List("Agelaius_phoeniceus")
+    val featureIndex = finalDf.columns.diff(ignored).map(finalDF.columns.indexOf(_))
+    val predictionIndex = finalDf.columns.indexOf("Agelaius_phoeniceus")
+
+    val finalDFRDD = finalDf.rdd.map(r => LabeledPoint(
+      r.getDouble(predictionIndex), // Get target value
+    //  // Map feature indices to values
+      Vectors.dense(featureIndex.map(r.getDouble(_)).toArray)
+      //Vectors.dense(featureIndex.map(r.getDouble(_)).toArray)
+    ))
+
+    //print(finalDFRDD)
+    val splits = finalDFRDD.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
+
+    val numClasses = 2
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val numTrees = 10 // Use more in practice.
+    val featureSubsetStrategy = "auto" // Let the algorithm choose.
+    val impurity = "gini"
+    val maxDepth = 9
+    val maxBins = 4000
+
+    val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+      numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+
+    //// Evaluate model on test instances and compute test error
+    val labelAndPreds = testData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
+    println("Test Error = " + testErr)
+    println("Learned classification forest model:\n" + model.toDebugString)
 
     // Save and load model
-    //model.save(sc, "target/tmp/myRandomForestRegressionModel")
-    //val sameModel = RandomForestModel.load(sc, "target/tmp/myRandomForestRegressionModel")
+    model.save(sc, "target/tmp/myRandomForestClassificationModel")
+    val sameModel = RandomForestModel.load(sc, "target/tmp/myRandomForestClassificationModel")
   }
   }
